ab7aaab31b9df21cddec541daecdf65c4f8b8786
Patrick Leary <122827401+Patrick-Leary@users.noreply.github.com>
2025-02-01 12:25:22 -0500

    Updated player prefab and locked z axis movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,6 +58,12 @@ public class PlayerController : MonoBehaviour
         {
             meleeTimer -= Time.deltaTime;
         }
+        HandleZAxis();
+    }
+
+    void HandleZAxis()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
     }
 
     void HandleGroundCheck()
