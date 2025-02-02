c3c341c8c61ec5e4fa0e8146797a36c86df71e39
llama-soup <43183037+llama-soup@users.noreply.github.com>
2025-02-01 19:39:43 -0500

    GOT THIS FUCKING HOURGLASS WORKING

--- Assets/Scripts/HourglassManager.cs
+++ Assets/Scripts/HourglassManager.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class HourglassManager : MonoBehaviour
@@ -5,6 +6,7 @@ public class HourglassManager : MonoBehaviour
 
     // Note: The material names are flipped in the editor so the material named bottom is actually the top material.
     public Material topMaterial;
+
     public Material bottomMaterial;
 
     public GameObject hourglass;
@@ -18,24 +20,42 @@ public class HourglassManager : MonoBehaviour
 
     public Material test;
 
+    Material[] materials;
+
+
+
     public void UpdateSoulMat(int currentSouls, int maxSouls)
     {
-        //Set Top Accordingly, .5 - 1 range
+
+        Material newTopInst = new Material(topMaterial);
+        Material newBottomInst = new Material(bottomMaterial);
+        //Set Top Accordingly, .16 - .3 range
+        ShaderUtil.allowAsyncCompilation = false;
 
         float currentSoulFloat = (float)currentSouls;
         float maxSoulFloat = (float)maxSouls;
 
-        float topConversion = ((currentSoulFloat/maxSoulFloat)/2f)+.5f;
-        topMaterial.SetFloat("soulEmptyAmount", topConversion);
-        hourglass.GetComponent<Renderer>().materials[3] = topMaterial;
+        
+
+        float topConversion = ((currentSoulFloat/maxSoulFloat)*.14f)+.16f;
+        newTopInst.SetFloat("_soulEmptyAmount", topConversion);
 
         
 
-        //Set Bottom Accordingly 0 - .5 range
-        float bottomConversion = .5f - ((currentSoulFloat/maxSoulFloat)/2f);
-        bottomMaterial.SetFloat("soulEmptyAmount", bottomConversion);
-        hourglass.GetComponent<Renderer>().materials[2] = test;
-        Debug.Log("Material's soulEmptyAmount: " + bottomMaterial.GetFloat("soulEmptyAmount"));
+
+        //Set Bottom Accordingly .01 - .16 range
+        float bottomConversion = .16f - ((currentSoulFloat/maxSoulFloat)*.15f) + .01f;
+        newBottomInst.SetFloat("_soulEmptyAmount", bottomConversion);
+        Debug.Log("Material's soulEmptyAmount: " + newBottomInst.GetFloat("_soulEmptyAmount"));
+
+        topMaterial.SetShaderPassEnabled("ForwardLit", true);
+        newBottomInst.SetShaderPassEnabled("ForwardLit", true);
+
+        materials = hourglass.GetComponent<Renderer>().materials;
+        materials[2] = newBottomInst;
+        materials[3] = newTopInst;
+
+        hourglass.GetComponent<Renderer>().materials = materials;
 
     }
 
