2ea51ce45e365cee47e82bbc54375fa692e3a1f6
Patrick Leary <122827401+Patrick-Leary@users.noreply.github.com>
2025-02-02 03:04:50 -0500

    updated game manager

--- Assets/Scripts/Killbox.cs
+++ Assets/Scripts/Killbox.cs
@@ -20,20 +20,9 @@ public class Killbox : MonoBehaviour
                 else{
                     gameManager.DamagePlayer(playerDamage);
                 }
-                
-                
-                // Handle CharacterController teleport
-                CharacterController controller = other.GetComponent<CharacterController>();
-                if (controller != null)
-                {
-                    controller.enabled = false;
-                    other.transform.position = gameManager.GetCurrentCheckpoint();
-                    controller.enabled = true;
-                }
-                else
-                {
-                    gameManager.KillPlayer();
-                }
+
+                gameManager.KillPlayer();
+            
             }
             else
             {
